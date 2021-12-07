fc2d4ebab6f9418631871e6ac4c6e8778c6f8f16
Mahesh <84697586+Dangetimahesh@users.noreply.github.com>
2021-12-07 16:05:47 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,90 @@
+##n=int(input())
+#data=list(map(int,input().split()))
+
+
+
+
+##n=int(input())
+#data=[]
+#for i in range(n):
+#    val=int(input())
+#    data.append(val)
+    
+
+
+
+
+##n=int(input())
+#data=list(map(int,input().split()))
+#print(*data)
+
+
+
+
+##n=int(input())
+#data=list(map(int,input().split()))
+#for i in range(n):
+#    print(i,data[i])
+
+
+
+
+
+##n=int(input())
+#data=list(map(int,input().split()))
+#for i in data:
+#    print(i)
+
+
+
+
+##dic={1:20,2:30,3:40,4:50}
+#for k in dic.keys():
+#   print(k,dic[k])
+
+
+
+
+##dic={1:20,2:30,3:40,4:50}
+#for v in dic.values():
+#    print(v)
+
+
+
+
+##dic={1:20,2:30,3:40,4:50}
+#for k,v in dic.items():
+#    print(k,v)
+
+
+
+
+#programes#
+n=int(input())
+data=list(map(int,input().split()))
+os=0
+es=0
+for i in data:
+    if i%2:
+        os+=i
+    else:
+        es+=i
+print(es,os)        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
